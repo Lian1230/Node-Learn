09070b79e63976e3a57b2648a9172cd67c8682b3
Lian <llian@seedlify.com>
2016-12-30 01:35:04 -0500

    webserver pass

--- frontend/entry.js
+++ frontend/entry.js
@@ -2,4 +2,14 @@ import 'bootstrap-webpack';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-console.log('hello, world');
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello,heheh</h1>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.body);
